4e415732eb773aad5a972ccdca12833edbabf73a
Alison Williams <apw5011@gmail.com>
2020-12-12 21:46:23 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -37,7 +37,9 @@ class MusicLibraryController
   
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
-    gets.strip 
+    input = gets.strip 
+    if artist = Artist.find_by_name(input)
+      
     
   end 
 end 
