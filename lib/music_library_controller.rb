112e03bb56711374d68c18fbe1b2f8bca0d496a6
Alison Williams <apw5011@gmail.com>
2020-12-12 21:28:27 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -24,6 +24,8 @@ class MusicLibraryController
   end 
   
   def list_artists
-    Artist.all.
+    Artist.all.sort {|a, b| a.name <=> b.name}.each.with_index(1) do |artist, index|
+      puts "#{index}. #{artist.name}"
+    end 
   end 
 end 
